510b074ff48b87a7664f13006256fa830ed923cb
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-04-22 20:02:28 +0530

    Author page text color changed, dp added

--- blog-site/src/pages/AuthorPage.js
+++ blog-site/src/pages/AuthorPage.js
@@ -8,7 +8,7 @@ const AuthorPage = () => {
       <div className="container">
         <div className="inner-container">
           <h1 className="page-title">About the Author: Abi</h1>
-          <img src="./img/me.jpg" className="hero-image" alt="" />
+          <img src="./" className="hero-image" alt="" />
           <div className="blog-content">
             <p className="blog-text">I believe that the main reason is that front-end development satisfies my need for creativity. It enabled me to easily put my ideas into digital form and get results back really quick. I still remember the first time I created an HTML page and opened it in the browser. A whole new world of possibilities appeared before my eyes. I became really passionate and kept creating since then. I’m really grateful that I’m able to make a living through it and made my passion my work. It absolutely has its ups and downs but if you love what you do and are able to provide value to people, the outcomes are far more rewarding! :-)</p>
           </div>
