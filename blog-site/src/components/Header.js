2f619ef4076a89edcec26545be0d4a8bc774d01e
Abm32 <110992125+Abm32@users.noreply.github.com>
2023-04-23 10:57:46 +0530

    Made the page responsive and added content

--- blog-site/src/components/Header.js
+++ blog-site/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <h1 className="header-title">Universe Unveiled</h1>
       <div className="header-sub">
         <Link className="header-buttons" to={"/"}>My Blogs</Link>
-        <Link className="header-buttons" to={"/author"}>About the Author</Link>
+        <Link className="header-buttons1" to={"/author"}>About the Author</Link>
       </div>
     </div>
   );
--- blog-site/src/pages/BlogPage.js
+++ blog-site/src/pages/BlogPage.js
@@ -16,9 +16,26 @@ const BlogPage = () => {
             <p className="blog-author">{my_blogs[blog_id].author}</p>
             <p className="blog-date">{my_blogs[blog_id].date}</p>
           </div>
-          <img src="./img/header.jpg" className="hero-image" alt="" />
+          <img src="./bgcolr.jpg" className="hero-image" alt="" />
           <div className="blog-content">
+            <h4 className="blog-text">{my_blogs[blog_id].title1}</h4>
             <p className="blog-text">{my_blogs[blog_id].content}</p>
+            <p className="blog-text">{my_blogs[blog_id].content1}</p>
+            <p className="blog-text">{my_blogs[blog_id].content2}</p>
+            <p className="blog-text">{my_blogs[blog_id].content3}</p>
+            <h4 className="blog-text">{my_blogs[blog_id].title2}</h4>
+            <p className="blog-text">{my_blogs[blog_id].content4}</p>
+            <p className="blog-text">{my_blogs[blog_id].content5}</p>
+            <p className="blog-text">{my_blogs[blog_id].content6}</p>
+            <p className="blog-text">{my_blogs[blog_id].content7}</p>
+            <h4 className="blog-text">{my_blogs[blog_id].title3}</h4>
+            <p className="blog-text">{my_blogs[blog_id].content8}</p>
+            <p className="blog-text">{my_blogs[blog_id].content9}</p>
+            <h4 className="blog-text">{my_blogs[blog_id].title4}</h4>
+            <p className="blog-text">{my_blogs[blog_id].content10}</p>
+            <p className="blog-text">{my_blogs[blog_id].content11}</p>
+            <p className="blog-text">{my_blogs[blog_id].content12}</p>
+            <p className="blog-text">{my_blogs[blog_id].content13}</p>
           </div>
         </div>
       </div>
